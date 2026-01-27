7fbeb8a36d2563df76e7e6c80d3fc2b43b14dbec
SaiMahesh-782 <saimahesh645@gmail.com>
2026-01-27 22:55:15 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+
+const express = require('express');
+
+
+const app = express();
+
+app.use("/hello",(req,res)=>{
+    res.send("Hello World from hello");
+});
+
+app.use("/test",(req,res)=>{
+    res.send("Hello World");
+})
+app.listen(3000,()=>{
+    console.log("Server is running on port 3000");
+})
